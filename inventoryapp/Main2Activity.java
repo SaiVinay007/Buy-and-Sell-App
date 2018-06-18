bcb59e17739b56c7fc4adb94d673f6b8fe9de72c
IITM SPW <tdave090@gmail.com>
2018-06-18 12:40:32 +0530

    second commit

--- inventoryapp/Main2Activity.java
+++ inventoryapp/Main2Activity.java
@@ -2,6 +2,8 @@ package com.ce17b019.inventoryapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Main2Activity extends AppCompatActivity {
 
@@ -9,5 +11,7 @@ public class Main2Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+        Button b1 = (Button) findViewById(R.id.button7);
+        b1.setVisibility(View.GONE);
     }
 }
